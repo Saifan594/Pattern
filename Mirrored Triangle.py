6139f8598689f77b284c1eb972af86be18703b2e
Saifan594 <saifanhasan72@gmail.com>
2024-10-24 18:36:44 +0600

    first commit

---
+++ Mirrored Triangle.py
@@ -0,0 +1,15 @@
+print( "\033c" )
+
+rows = int( input( "enter the number of Rows : " ) )
+
+for i in range( 1, rows + 1 ):
+    
+    for j in range( 0, rows - i ):
+        
+        print( " ", end = " " )
+    
+    for j in range( 1, i + 1 ):
+        
+        print( "*", end = " " )
+    
+    print()
